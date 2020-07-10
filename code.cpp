7e6073d9767d1f9f2d8e7709196f4b0cbdcbb48a
wtl666wtl <2351090351@qq.com>
2020-07-10 18:54:15 +0800

    Add files via upload

--- code.cpp
+++ code.cpp
@@ -3,10 +3,11 @@
 #include <cstring>
 #include <iostream>
 #define N 1000006
+#define uint unsigned int
 using namespace std;
-char s[16];bool IR[32];
-const int endline[32]={0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,1,0,0,1,1};
-int pos,PC,NPC,rd,rs1,rs2,reg[32],A,B,C,fun,fun2,Imm,Op,LMD,cond,ALUOut,Mem[N];
+char s[16];
+int pos,PC,MEM_num,Mem[N],reg[32],reg_num[32],fc[N];
+bool WB_oc,MEM_oc,EX_oc,ID_oc,isend;
 int getpos()
 {
 	int p=0;
@@ -15,7 +16,7 @@ int getpos()
 		else p=p*16+s[i]-'0';
 	return p;
 }
-int getnum()//binary
+int getnum()
 {
 	int x=0,y=0,num=0;
 	if(s[1]>='A')y=s[1]-'A'+10;else y=s[1]-'0';
@@ -32,141 +33,190 @@ int getnum()//binary
 	}
 	return num;
 }
+struct relay{
+	int Imm,fun,fun2,rd,rs1,rs2;
+	relay(){
+		Imm=fun=fun2=rd=rs1=rs2=0;
+	}
+}tmp;
+struct date{
+	bool IR[32],forecast;
+	int Op,Imm,fun,fun2,rd,rs1,rs2,pc,A,B,C,M;
+	date(){
+		memset(IR,0,sizeof IR);
+		forecast=Op=Imm=fun=rd=rs1=rs2=pc=A=B=C=M=0;
+	}
+}IF_ID,ID_EX,EX_MEM,MEM_WB;
 void preU()
 {
-	rd=0;
-	for(int i=11;i>=7;i--)rd=rd*2+IR[i];
-	Imm=0;
-	for(int i=31;i>=12;i--)Imm=Imm*2+IR[i];
-	Imm<<=12;
+	tmp.rd=0;
+	for(int i=11;i>=7;i--)tmp.rd=tmp.rd*2+IF_ID.IR[i];
+	tmp.Imm=0;
+	for(int i=31;i>=12;i--)tmp.Imm=tmp.Imm*2+IF_ID.IR[i];
+	tmp.Imm<<=12;
 }
 void preJ()
 {
-	rd=0;
-	for(int i=11;i>=7;i--)rd=rd*2+IR[i];
-	Imm=0;
-	for(int i=31;i>=20;i--)Imm=Imm*2+IR[31];
-	for(int i=19;i>=12;i--)Imm=Imm*2+IR[i];
-	Imm=Imm*2+IR[20];
-	for(int i=10;i>=1;i--)Imm=Imm*2+IR[i+20];
-	Imm<<=1;
+	tmp.rd=0;
+	for(int i=11;i>=7;i--)tmp.rd=tmp.rd*2+IF_ID.IR[i];
+	tmp.Imm=0;
+	for(int i=31;i>=20;i--)tmp.Imm=tmp.Imm*2+IF_ID.IR[31];
+	for(int i=19;i>=12;i--)tmp.Imm=tmp.Imm*2+IF_ID.IR[i];
+	tmp.Imm=tmp.Imm*2+IF_ID.IR[20];
+	for(int i=10;i>=1;i--)tmp.Imm=tmp.Imm*2+IF_ID.IR[i+20];
+	tmp.Imm<<=1;
 }
 void preI()
 {
-	rd=0;
-	for(int i=11;i>=7;i--)rd=rd*2+IR[i];
-	fun=0;
-	for(int i=14;i>=12;i--)fun=fun*10+IR[i];
-	rs1=0;
-	for(int i=19;i>=15;i--)rs1=rs1*2+IR[i];
-	Imm=0;
-	for(int i=31;i>=11;i--)Imm=Imm*2+IR[31];
-	for(int i=10;i>=0;i--)Imm=Imm*2+IR[i+20];
+	tmp.rd=0;
+	for(int i=11;i>=7;i--)tmp.rd=tmp.rd*2+IF_ID.IR[i];
+	tmp.fun=0;
+	for(int i=14;i>=12;i--)tmp.fun=tmp.fun*10+IF_ID.IR[i];
+	tmp.rs1=0;
+	for(int i=19;i>=15;i--)tmp.rs1=tmp.rs1*2+IF_ID.IR[i];
+	tmp.Imm=0;
+	for(int i=31;i>=11;i--)tmp.Imm=tmp.Imm*2+IF_ID.IR[31];
+	for(int i=10;i>=0;i--)tmp.Imm=tmp.Imm*2+IF_ID.IR[i+20];
 }
 void preB()
 {
-	fun=0;
-	for(int i=14;i>=12;i--)fun=fun*10+IR[i];
-	rs1=0;
-	for(int i=19;i>=15;i--)rs1=rs1*2+IR[i];
-	rs2=0;
-	for(int i=24;i>=20;i--)rs2=rs2*2+IR[i];
-	Imm=0;
-	for(int i=31;i>=12;i--)Imm=Imm*2+IR[31];
-	Imm=Imm*2+IR[7];
-	for(int i=10;i>=5;i--)Imm=Imm*2+IR[i+20];
-	for(int i=4;i>=1;i--)Imm=Imm*2+IR[i+7];
-	Imm<<=1;
+	tmp.fun=0;
+	for(int i=14;i>=12;i--)tmp.fun=tmp.fun*10+IF_ID.IR[i];
+	tmp.rs1=0;
+	for(int i=19;i>=15;i--)tmp.rs1=tmp.rs1*2+IF_ID.IR[i];
+	tmp.rs2=0;
+	for(int i=24;i>=20;i--)tmp.rs2=tmp.rs2*2+IF_ID.IR[i];
+	tmp.Imm=0;
+	for(int i=31;i>=12;i--)tmp.Imm=tmp.Imm*2+IF_ID.IR[31];
+	tmp.Imm=tmp.Imm*2+IF_ID.IR[7];
+	for(int i=10;i>=5;i--)tmp.Imm=tmp.Imm*2+IF_ID.IR[i+20];
+	for(int i=4;i>=1;i--)tmp.Imm=tmp.Imm*2+IF_ID.IR[i+7];
+	tmp.Imm<<=1;
 }
 void preS()
 {
-	fun=0;
-	for(int i=14;i>=12;i--)fun=fun*10+IR[i];
-	rs1=0;
-	for(int i=19;i>=15;i--)rs1=rs1*2+IR[i];
-	rs2=0;
-	for(int i=24;i>=20;i--)rs2=rs2*2+IR[i];
-	Imm=0;
-	for(int i=31;i>=11;i--)Imm=Imm*2+IR[31];
-	for(int i=10;i>=5;i--)Imm=Imm*2+IR[i+20];
-	for(int i=4;i>=0;i--)Imm=Imm*2+IR[i+7];
+	tmp.fun=0;
+	for(int i=14;i>=12;i--)tmp.fun=tmp.fun*10+IF_ID.IR[i];
+	tmp.rs1=0;
+	for(int i=19;i>=15;i--)tmp.rs1=tmp.rs1*2+IF_ID.IR[i];
+	tmp.rs2=0;
+	for(int i=24;i>=20;i--)tmp.rs2=tmp.rs2*2+IF_ID.IR[i];
+	tmp.Imm=0;
+	for(int i=31;i>=11;i--)tmp.Imm=tmp.Imm*2+IF_ID.IR[31];
+	for(int i=10;i>=5;i--)tmp.Imm=tmp.Imm*2+IF_ID.IR[i+20];
+	for(int i=4;i>=0;i--)tmp.Imm=tmp.Imm*2+IF_ID.IR[i+7];
 }
 void preR()
 {
-	rd=0;
-	for(int i=11;i>=7;i--)rd=rd*2+IR[i];
-	fun=0;
-	for(int i=14;i>=12;i--)fun=fun*10+IR[i];
-	rs1=0;
-	for(int i=19;i>=15;i--)rs1=rs1*2+IR[i];
-	rs2=0;
-	for(int i=24;i>=20;i--)rs2=rs2*2+IR[i];
-	fun2=0;
-	for(int i=31;i>=25;i--)fun2=fun2*10+IR[i];
+	tmp.rd=0;
+	for(int i=11;i>=7;i--)tmp.rd=tmp.rd*2+IF_ID.IR[i];
+	tmp.fun=0;
+	for(int i=14;i>=12;i--)tmp.fun=tmp.fun*10+IF_ID.IR[i];
+	tmp.rs1=0;
+	for(int i=19;i>=15;i--)tmp.rs1=tmp.rs1*2+IF_ID.IR[i];
+	tmp.rs2=0;
+	for(int i=24;i>=20;i--)tmp.rs2=tmp.rs2*2+IF_ID.IR[i];
+	tmp.fun2=0;
+	for(int i=31;i>=25;i--)tmp.fun2=tmp.fun2*10+IF_ID.IR[i];
 }
-int judge()
+void deal_fc(int pc,int f)
 {
-	if(Op==110111)return preU(),1;
-	if(Op==10111)return preU(),2;
-	if(Op==1101111)return preJ(),3;
-	if(Op==1100111)return preI(),4;
-	if(Op==1100011)return preB(),5;
-	if(Op==11)return preI(),6;
-	if(Op==100011)return preS(),7;
-	if(Op==10011)return preI(),8;
-	if(Op==110011)return preR(),9;
-}
-bool pd()
-{
-	if(fun==0){
-		return B==C;
-	}else if(fun==1){
-		return B!=C;
-	}else if(fun==100){
-		return B<C;
-	}else if(fun==101){
-		return B>=C;
-	}else if(fun==110){
-		return (unsigned int)B<(unsigned int)C;
-	}else if(fun==111){
-		return (unsigned int)B>=(unsigned int)C;
+	if(f){
+		fc[pc]=min(fc[pc]+1,3);
+	}else{
+		fc[pc]=max(fc[pc]-1,0);
 	}
 }
-void doOp6()
+void doOp5(date x)
 {
-	LMD=0;
+	int fun=x.fun,B=x.B,C=x.C,pc=x.pc,Imm=x.Imm;
 	if(fun==0){
-		for(int i=31;i>=24;i--)LMD=LMD*2+Mem[B+Imm]/(1<<7);
-		int t=Mem[B+Imm];
+		if(B==C){
+			if(!x.forecast)PC=pc+Imm,ID_oc=0;
+			deal_fc(x.pc,1);
+		}else{
+			if(x.forecast)PC=pc+4,ID_oc=0;
+			deal_fc(x.pc,0);
+		}
+	}else if(fun==1){
+		if(B!=C){
+			if(!x.forecast)PC=pc+Imm,ID_oc=0;
+			deal_fc(x.pc,1);
+		}else{
+			if(x.forecast)PC=pc+4,ID_oc=0;
+			deal_fc(x.pc,0);
+		}
+	}else if(fun==100){
+		if(B<C){
+			if(!x.forecast)PC=pc+Imm,ID_oc=0;
+			deal_fc(x.pc,1);
+		}else{
+			if(x.forecast)PC=pc+4,ID_oc=0;
+			deal_fc(x.pc,0);
+		}
+	}else if(fun==101){
+		if(B>=C){
+			if(!x.forecast)PC=pc+Imm,ID_oc=0;
+			deal_fc(x.pc,1);
+		}else{
+			if(x.forecast)PC=pc+4,ID_oc=0;
+			deal_fc(x.pc,0);
+		}
+	}else if(fun==110){
+		if((uint)B<(uint)C){
+			if(!x.forecast)PC=pc+Imm,ID_oc=0;
+			deal_fc(x.pc,1);
+		}else{
+			if(x.forecast)PC=pc+4,ID_oc=0;
+			deal_fc(x.pc,0);
+		}
+	}else if(fun==111){
+		if((uint)B>=(uint)C){
+			if(!x.forecast)PC=pc+Imm,ID_oc=0;
+			deal_fc(x.pc,1);
+		}else{
+			if(x.forecast)PC=pc+4,ID_oc=0;
+			deal_fc(x.pc,0);
+		}
+	}
+}
+int doOp6(date x)
+{
+	int LMD=0;
+	int fun=x.fun,M=x.M;
+	if(fun==0){
+		for(int i=31;i>=24;i--)LMD=LMD*2+Mem[M]/(1<<7);
+		int t=Mem[M];
 		for(int i=1<<7;i>=1;i>>=1)LMD=LMD*2+t/i,t%=i;
 	}else if(fun==1){
-		for(int i=31;i>=16;i--)LMD=LMD*2+Mem[B+Imm+1]/(1<<7);
-		int t=Mem[B+Imm+1];
+		for(int i=31;i>=16;i--)LMD=LMD*2+Mem[M+1]/(1<<7);
+		int t=Mem[M+1];
 		for(int i=1<<7;i>=1;i>>=1)LMD=LMD*2+t/i,t%=i;
-		t=Mem[B+Imm];
+		t=Mem[M];
 		for(int i=1<<7;i>=1;i>>=1)LMD=LMD*2+t/i,t%=i;
 	}else if(fun==10){
-		int t=Mem[B+Imm+3];
+		int t=Mem[M+3];
 		for(int i=1<<7;i>=1;i>>=1)LMD=LMD*2+t/i,t%=i;
-		t=Mem[B+Imm+2];
+		t=Mem[M+2];
 		for(int i=1<<7;i>=1;i>>=1)LMD=LMD*2+t/i,t%=i;
-		t=Mem[B+Imm+1];
+		t=Mem[M+1];
 		for(int i=1<<7;i>=1;i>>=1)LMD=LMD*2+t/i,t%=i;
-		t=Mem[B+Imm];
+		t=Mem[M];
 		for(int i=1<<7;i>=1;i>>=1)LMD=LMD*2+t/i,t%=i;
 	}else if(fun==100){
-		int t=Mem[B+Imm];
+		int t=Mem[M];
 		for(int i=1<<7;i>=1;i>>=1)LMD=LMD*2+t/i,t%=i;
 	}else if(fun==101){
-		int t=Mem[B+Imm+1];
+		int t=Mem[M+1];
 		for(int i=1<<7;i>=1;i>>=1)LMD=LMD*2+t/i,t%=i;
-		t=Mem[B+Imm];
+		t=Mem[M];
 		for(int i=1<<7;i>=1;i>>=1)LMD=LMD*2+t/i,t%=i;
 	}
+	return LMD;
 }
-void doOp7()
+void doOp7(date x)
 {
-	int t=ALUOut;
+	int t=x.M,fun=x.fun,C=x.C;
 	if(fun==0){
 		int tmp=(unsigned int)C%(1<<8);
 		Mem[t]=tmp;
@@ -186,8 +236,10 @@ void doOp7()
 		Mem[t+3]=tmp;
 	}
 }
-void doOp8()
+int doOp8(date x)
 {
+	int ALUOut=0;
+	int B=x.B,Imm=x.Imm,fun=x.fun;
 	if(fun==0){
 		ALUOut=B+Imm;
 	}else if(fun==10){
@@ -211,9 +263,12 @@ void doOp8()
 			for(int i=0;i<Imm;i++)ALUOut+=(1<<(31-i));
 		}
 	}
+	return ALUOut;
 }
-void doOp9()
+int doOp9(date x)
 {
+	int ALUOut=0;
+	int B=x.B,Imm=x.Imm,fun=x.fun,C=x.C,fun2=x.fun2;
 	if(fun==0&&fun2==0){
 		ALUOut=B+C;
 	}else if(fun==0&&fun2!=0){
@@ -238,107 +293,144 @@ void doOp9()
 	}else if(fun==111){
 		ALUOut=B&C;
 	}
+	return ALUOut;
+}
+int judge(int Op)
+{
+	if(Op==110111)return preU(),1;
+	if(Op==10111)return preU(),2;
+	if(Op==1101111)return preJ(),3;
+	if(Op==1100111)return preI(),4;
+	if(Op==1100011)return preB(),5;
+	if(Op==11)return preI(),6;
+	if(Op==100011)return preS(),7;
+	if(Op==10011)return preI(),8;
+	if(Op==110011)return preR(),9;
 }
 void IF()
 {
+	if(ID_oc)return;
 	if(Mem[PC]==19&&Mem[PC+1]==5&&Mem[PC+2]==240&&Mem[PC+3]==15){
-		printf("%u\n",reg[10]&255u);
-		exit(0);
+		isend=1;
+		return;
 	}
+	IF_ID=date();
 	int t=Mem[PC+3],p=24;
-	memset(IR,0,sizeof(IR));
 	while(t){
-		if(t&1)IR[p]=1;
+		if(t&1)IF_ID.IR[p]=1;
 		t>>=1;p++;
 	}
 	t=Mem[PC+2],p=16;
 	while(t){
-		if(t&1)IR[p]=1;
+		if(t&1)IF_ID.IR[p]=1;
 		t>>=1;p++;
 	}
 	t=Mem[PC+1],p=8;
 	while(t){
-		if(t&1)IR[p]=1;
+		if(t&1)IF_ID.IR[p]=1;
 		t>>=1;p++;
 	}
 	t=Mem[PC],p=0;
 	while(t){
-		if(t&1)IR[p]=1;
+		if(t&1)IF_ID.IR[p]=1;
 		t>>=1;p++;
 	}
-	NPC=PC+4;
-	//for(int i=31;i>=0;i--)printf("%d",IR[i]);puts("");
+	IF_ID.pc=PC;PC+=4;
+	ID_oc=1;
 }
 void ID()
 {
-	Op=0;
-	for(int i=6;i>=0;i--)Op=Op*10+IR[i];
-	Op=judge();
-	A=reg[rd];
-	B=reg[rs1];
-	C=reg[rs2];
-//	printf("%d %d %d  %d %d %d %d %d\n",Op,PC,rd,rs1,rs2,B,C,Imm);
+	if(!ID_oc||EX_oc)return;
+	int op=0;
+	for(int i=6;i>=0;i--)op=op*10+IF_ID.IR[i];
+	tmp=relay();
+	op=judge(op);
+	if(tmp.rs1&&reg_num[tmp.rs1]||tmp.rs2&&reg_num[tmp.rs2])return;
+	ID_oc=0;EX_oc=1;
+	if(tmp.rd)reg_num[tmp.rd]++;
+	ID_EX=IF_ID;
+	ID_EX.Op=op;
+	//Imm,fun,fun2,rd,rs1,rs2
+	ID_EX.Imm=tmp.Imm;
+	ID_EX.fun=tmp.fun;
+	ID_EX.fun2=tmp.fun2;
+	ID_EX.rd=tmp.rd;
+	ID_EX.rs1=tmp.rs1;
+	ID_EX.rs2=tmp.rs2;
+	ID_EX.B=reg[tmp.rs1];
+	ID_EX.C=reg[tmp.rs2];
+	if(op==3){
+		PC=ID_EX.pc+ID_EX.Imm;
+	}else if(op==4){
+		PC=(ID_EX.B+ID_EX.Imm)&~1;
+	}else if(op==5){
+		ID_EX.forecast=fc[ID_EX.pc]&2;
+		if(ID_EX.forecast)PC=ID_EX.pc+ID_EX.Imm;
+	}
+//	printf("%d %d %d  %d %d %d %d %d\n",op,ID_EX.pc,tmp.rd,tmp.rs1,tmp.rs2,ID_EX.B,ID_EX.C,ID_EX.Imm);
 }
 void EX()
 {
-	if(Op==1){
-		ALUOut=Imm;
-	}else if(Op==2){
-		ALUOut=PC+Imm;
-	}else if(Op==3){
-		ALUOut=PC+4;
-		NPC=PC+Imm;
-	}else if(Op==4){
-		ALUOut=PC+4;
-		NPC=(B+Imm)&~1;
-	}else if(Op==5){
-		cond=pd();
-		ALUOut=PC+Imm;
-	}else if(Op==6){
-		//doOp6();
-		//printf("#### %d %d\n",Mem[B+Imm],Mem[B+Imm+1]);
-	}else if(Op==7){
-		ALUOut=B+Imm;
-	}else if(Op==8){
-		doOp8();
-	}else if(Op==9){
-		doOp9();
+	if(!EX_oc||MEM_oc)return;
+	EX_MEM=ID_EX;
+	MEM_num=EX_oc=0;MEM_oc=1;
+	int op=ID_EX.Op;
+	if(op==1){
+		EX_MEM.A=ID_EX.Imm;
+	}else if(op==2){
+		EX_MEM.A=ID_EX.pc+ID_EX.Imm;
+	}else if(op==3||op==4){
+		EX_MEM.A=ID_EX.pc+4;
+	}else if(op==5){
+		doOp5(ID_EX);
+	}else if(op==6||op==7){
+		EX_MEM.M=ID_EX.B+ID_EX.Imm;
+	}else if(op==8){
+		EX_MEM.A=doOp8(ID_EX);
+	}else if(op==9){
+		EX_MEM.A=doOp9(ID_EX);
 	}
 }
 void MEM()
 {
-	PC=NPC;
-	if(Op==6){
-		doOp6();
-	}else if(Op==5){
-		if(cond)PC=ALUOut;
-	}else if(Op==7){
-		doOp7();
+	if(!MEM_oc||WB_oc)return;
+	MEM_WB=EX_MEM;
+	int op=EX_MEM.Op;
+	if(op==6||op==7){
+		if(MEM_num<0)MEM_num++;
+		else{
+			if(op==6){
+				MEM_WB.A=doOp6(EX_MEM);
+			}else{
+				doOp7(EX_MEM);
+			}
+			MEM_oc=0;WB_oc=1;
+		}
+	}else{
+		MEM_oc=0;WB_oc=1;
 	}
 }
 void WB()
 {
-	if(Op==1||Op==2||Op==3||Op==4||Op==8||Op==9){
-		reg[rd]=ALUOut;
-	}else if(Op==6){
-		reg[rd]=LMD;
+	if(!WB_oc)return;
+	WB_oc=0;
+	int op=MEM_WB.Op;
+	if(op!=5&&op!=7&&MEM_WB.rd){
+		reg[MEM_WB.rd]=MEM_WB.A;
+		reg_num[MEM_WB.rd]--;
 	}
-	reg[0]=0;
 }
 int main()
 {
-//	freopen("bulgarian.data","r",stdin);
+//	freopen("pi.data","r",stdin);
 	while(scanf("%s",s)!=EOF){
 		if(s[0]=='@')pos=getpos();
 		else Mem[pos]=getnum(),pos++;
 	}
-	while(1){
-		IF();
-		ID();
-		EX();
-		MEM();
-		WB();
-	//	printf("%u %u\n",(unsigned int)(reg[10]),(unsigned int)(reg[11]));
-	//	printf("%u %u\n",(unsigned int)(Mem[4480]),(unsigned int)(Mem[4481]));
+	while(!isend||WB_oc||MEM_oc||EX_oc||ID_oc){
+		WB(),MEM(),EX(),ID(),IF();
+	//	printf("%d %d %d %d  %d\n",ID_oc,EX_oc,MEM_oc,WB_oc,MEM_num);
+		//for(int i=0;i<=31;i++)printf("%d ",reg_num[i]);puts("");
 	}
+	printf("%u\n",(uint)reg[10]&255u);
 }
